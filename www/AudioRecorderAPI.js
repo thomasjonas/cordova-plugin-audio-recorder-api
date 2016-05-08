62614c3919dbffa37d809c2ce9a4e3d791ee59c3
Thomas Boland <thomas@thomasjonas.nl>
2016-05-08 22:38:45 +0200

    JS fixes

--- www/AudioRecorderAPI.js
+++ www/AudioRecorderAPI.js
@@ -3,14 +3,14 @@ function AudioRecorderAPI() {
 
 AudioRecorderAPI.prototype.record = function (successCallback, errorCallback, options) {
   // define options
-  var duration = options.duration || null;
+  var duration = options.duration || 30;
   var sampleRate = options.sampleRate || 16000.0;
   var bitRate = options.bitRate || 12000;
   var bitDepth = options.bitDepth || 16; // 8, 16, 24, or 32.
   var numberOfChannels = options.numberOfChannels || 1;
   var audioQuality = options.audioQuality || 'medium'; // min, low, medium, high or max.
 
-  cordova.exec(successCallback, errorCallback, "AudioRecorderAPI", "record", [duration, sampleRate, bitRate, bitDepth, numberOfChannels]);
+  cordova.exec(successCallback, errorCallback, "AudioRecorderAPI", "record", [duration, sampleRate, bitRate, bitDepth, numberOfChannels, audioQuality]);
 };
 
 AudioRecorderAPI.prototype.stop = function (successCallback, errorCallback) {
